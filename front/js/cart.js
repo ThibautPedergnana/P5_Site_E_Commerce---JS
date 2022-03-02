f7117c7699a91717f676ab012a1e4e2bd453581b
Thibaut Pedergnana <t.pedergnana@Outlook.fr>
2022-03-02 19:27:05 +0100

    first commit

---
+++ front/js/cart.js
@@ -0,0 +1,95 @@
+// Initialisation du local storage
+let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+console.table(produitLocalStorage);
+const positionEmptyCart = document.querySelector("#cart__items");
+
+// Si le panier est vide
+function getCart() {
+  if (produitLocalStorage === null || produitLocalStorage == 0) {
+    const emptyCart = `<p>Votre panier est vide</p>`;
+    positionEmptyCart.innerHTML = emptyCart;
+  } else {
+    for (let produit in produitLocalStorage) {
+      // Insertion de l'élément "article"
+      let productArticle = document.createElement("article");
+      document.querySelector("#cart__items").appendChild(productArticle);
+      productArticle.className = "cart__item";
+      productArticle.setAttribute(
+        "data-id",
+        produitLocalStorage[produit].idProduit
+      );
+
+      // Insertion de l'élément "div"
+      let productDiv = document.createElement("div");
+      productArticle.appendChild(productDiv);
+      productDiv.className = "cart__item__img";
+      console.log(produitLocalStorage.imgProduit);
+      // Insertion de l'image
+      let productImg = document.createElement("img");
+      productDiv.appendChild(productImg);
+      productImg.src = produitLocalStorage[produit].imgProduit;
+      productImg.alt = produitLocalStorage[produit].altProduit;
+
+      // Insertion de l'élément "div"
+      let productItemContent = document.createElement("div");
+      productArticle.appendChild(productItemContent);
+      productItemContent.className = "cart__item__content";
+
+      // Insertion de l'élément "div"
+      let productItemContentTitlePrice = document.createElement("div");
+      productItemContent.appendChild(productItemContentTitlePrice);
+      productItemContentTitlePrice.className =
+        "cart__item__content__titlePrice";
+
+      // Insertion du titre h2
+      let productTitle = document.createElement("h2");
+      productItemContentTitlePrice.appendChild(productTitle);
+      productTitle.innerHTML = produitLocalStorage[produit].nomProduit;
+
+      // Insertion de la couleur
+      let productColor = document.createElement("p");
+      productTitle.appendChild(productColor);
+      productColor.innerHTML = produitLocalStorage[produit].couleurProduit;
+
+      // Insertion du prix
+      let productPrice = document.createElement("p");
+      productItemContentTitlePrice.appendChild(productPrice);
+      productPrice.innerHTML = produitLocalStorage[produit].prixProduit;
+
+      // Insertion de l'élément "div"
+      let productItemContentSettings = document.createElement("div");
+      productItemContent.appendChild(productItemContentSettings);
+      productItemContentSettings.className = "cart__item__content__settings";
+
+      // Insertion de l'élément "div"
+      let productItemContentSettingsQuantity = document.createElement("div");
+      productItemContent.appendChild(productItemContentSettingsQuantity);
+      productItemContentSettings.className =
+        "cart__item__content__settings__quantity";
+
+      // Insertion de Qte
+      let productQte = document.createElement("p");
+      productItemContentSettingsQuantity.appendChild(productQte);
+      productQte.innerHTML = "Qté :";
+
+      // Insertion de l'élément "input"
+      let productQuantity = document.createElement("input");
+      productItemContentSettingsQuantity.appendChild(productQuantity);
+      productQuantity.value = produitLocalStorage[produit].quantiteProduit;
+      productQuantity.className = "itemQuantity";
+
+      // Insertion de l'élément "div"
+      let productItemContentSettingsDelete = document.createElement("div");
+      productItemContentSettings.appendChild(productItemContentSettingsDelete);
+      productItemContentSettingsDelete.className =
+        "cart__item__content__settings__delete";
+
+      // Instertion de l'élément "p"
+      let productSupprimer = document.createElement("p");
+      productItemContentSettingsDelete.appendChild(productSupprimer);
+      productSupprimer.className = "deleteItem";
+      productSupprimer.innerHTML = "Supprimer";
+    }
+  }
+}
+getCart();
---
+++ front/js/product.js
@@ -0,0 +1,122 @@
+let str = window.location.href;
+let url = new URL(str);
+let idProduct = url.searchParams.get("id");
+
+let article = "";
+
+const colorPicked = document.querySelector("#colors");
+const quantityPicked = document.querySelector("#quantity");
+
+getArticle();
+
+// Récupération des articles de l'API
+function getArticle() {
+  fetch("http://localhost:3000/api/products/" + idProduct)
+    .then((res) => {
+      return res.json();
+    })
+
+    .then(async function (resultatAPI) {
+      article = await resultatAPI;
+      if (article) {
+        getPost(article);
+      }
+    })
+
+    .catch((error) => {
+      console.log("Erreur de la requête");
+    });
+}
+
+function getPost(article) {
+  // Insertion de l'image
+  let productImg = document.createElement("img");
+  document.querySelector(".item__img").appendChild(productImg);
+  productImg.src = article.imageUrl;
+  productImg.alt = article.altTxt;
+
+  // Modification du titre "h1"
+  let productName = document.getElementById("title");
+  productName.innerHTML = article.name;
+
+  // Modification du prix
+  let productPrice = document.getElementById("price");
+  productPrice.innerHTML = article.price;
+
+  // Modification de la description
+  let productDescription = document.getElementById("description");
+  productDescription = article.description;
+
+  // Insertion des options de couleurs
+  for (let colors of article.colors) {
+    console.table(colors);
+    let productColors = document.createElement("option");
+    document.querySelector("#colors").appendChild(productColors);
+    productColors.value = colors;
+    productColors.innerHTML = colors;
+  }
+  addToCart(article);
+}
+
+// Ajouter au panier
+function addToCart(article) {
+  const btn_envoyerPanier = document.querySelector("#addToCart");
+
+  // Valider les conditions d'ajout au panier
+  btn_envoyerPanier.addEventListener("click", (event) => {
+    if (quantityPicked.value > 0 && quantityPicked.value <= 100) {
+      // Choix de couleur
+      let choixCouleur = colorPicked.value;
+
+      // Choix de la quantité
+      let choixQuantite = quantityPicked.value;
+
+      let optionsProduit = {
+        idProduit: idProduct,
+        couleurProduit: choixCouleur,
+        quantiteProduit: Number(choixQuantite),
+        nomProduit: article.name,
+        prixProduit: article.price,
+        descriptionProduit: article.description,
+        imgProduit: article.imageUrl,
+        altProduit: article.altTxt,
+      };
+      // Initialisation du local storage
+      let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+
+      // Fenêtre pop-up
+      const popupConfirmation = () => {
+        if (
+          window.confirm(`Votre commande de ${choixQuantite} ${article.name} ${choixCouleur} est ajoutée au panier
+        Pour consulter votre panier, cliquez sur OK`)
+        ) {
+          window.location.href = "cart.html";
+        }
+      };
+
+      // Importer dans le local storage
+      // Si le panier comporte déjà au moins 1 article
+      if (produitLocalStorage) {
+        const resultFind = produitLocalStorage.find(
+          (el) =>
+            el.idProduit === idProduct && el.couleurProduit === choixCouleur
+        );
+        // Si le produit commandé est déjà dans le panier
+        if (resultFind) {
+          // Si le produit n'est pas dans le panier
+        } else {
+          produitLocalStorage.push(optionsProduit);
+          localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+          popupConfirmation();
+        }
+        // Si le panier est vide
+      } else {
+        produitLocalStorage = [];
+        produitLocalStorage.push(optionsProduit);
+        localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+        console.table(produitLocalStorage);
+        popupConfirmation();
+      }
+    }
+  });
+}
