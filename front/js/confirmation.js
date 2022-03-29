0be65bdb52bf9d181d40a00d043300b3cc59e309
Thibaut Pedergnana <t.pedergnana@Outlook.fr>
2022-03-29 17:58:44 +0200

    confirmation order

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,7 +1,6 @@
-function main() {
-  const idNode = document.getElementById("orderId");
-  idNode.innerText = localStorage.getItem("orderId");
-  localStorage.clear();
-}
+// Récupération orderID
+const params = new URL(document.location).searchParams;
+const orderId = params.get("orderId");
 
-main();
+// Insertion du numéro de commande
+document.getElementById("orderId").textContent = orderId;
